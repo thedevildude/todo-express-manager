d31d23f0d090e709a9803458aaba2f90f54ad10d
Devdeep Ghosh <devarmforce01@gmail.com>
2022-12-13 05:44:13 +0530

    todo test jest

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,30 +1,46 @@
-const request = require('supertest')
-const db = require('../models/index')
-const app = require('../app')
+const request = require("supertest");
+const db = require("../models/index");
+const app = require("../app");
 
-let server, agent
+let server, agent;
 
-describe('Todo test suite', () => {
+describe("Todo test suite", () => {
   beforeAll(async () => {
-    await db.sequelize.sync({ force: true })
-    server = app.listen(3000, () => { })
-    agent = request.agent(server)
-  })
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
   afterAll(async () => {
-    await db.sequelize.close()
-    server.close()
-  })
-  test('respond with a json at /todos', async () => {
-    const response = await agent.post('/todos').send({
-      title: 'Buy Milk',
+    await db.sequelize.close();
+    server.close();
+  });
+  test("respond with a json at /todos", async () => {
+    const response = await agent.post("/todos").send({
+      title: "Buy Milk",
       dueDate: new Date().toISOString(),
-      completed: false
-    })
-    expect(response.statusCode).toBe(200)
-    expect(response.header['content-type']).toBe(
-      'application/json; charset=utf-8'
-    )
-    const parsedResponse = JSON.parse(response.text)
-    expect(parsedResponse.id).toBeDefined()
-  })
-})
+      completed: false,
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.header["content-type"]).toBe(
+      "application/json; charset=utf-8"
+    );
+    const parsedResponse = JSON.parse(response.text);
+    expect(parsedResponse.id).toBeDefined();
+  });
+
+  test("Mark a Todo as complete", async () => {
+    const response = await agent.post("/todos").send({
+      title: "Buy Milk and Water",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    const parsedResponse = JSON.parse(response.text);
+    const todoID = parsedResponse.id;
+    expect(parsedResponse.completed).toBe(false);
+    const markCompleteResponse = await agent
+      .put(`/todos/${todoID}/markAsCompleted`)
+      .send();
+    const parsedUpdateResponse = JSON.parse(markCompleteResponse.text);
+    expect(parsedUpdateResponse.completed).toBe(true);
+  });
+});
--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -14,20 +12,23 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static addTodo({ title, dueDate }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false })
+      return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
     markAsCompleted() {
-      return this.update({ completed: true })
+      return this.update({ completed: true });
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
   return Todo;
 };
