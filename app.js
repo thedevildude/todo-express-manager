3e845e8924665e8a96a7ff450b55e61838a4c955
Devdeep Ghosh <devarmforce01@gmail.com>
2022-12-25 01:29:55 +0530

    Added tiny csrf

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require("path");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-var csurf = require("csurf");
+var csrf = require("tiny-csrf");
 var cookieParser = require("cookie-parser");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("shh! some secret string"));
-app.use(csurf({ cookie: true }));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 
 // Set ejs as view engine
 app.set("view engine", "ejs");
